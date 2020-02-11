8585d4f21b06715d1d349325fc211b969329710b
yasmine.morawej@gmail.com <yasmine.morawej@gmail.com>
2020-02-11 11:49:46 +0000

    set up app.rb page and inherit base class from Sinatra

--- app.rb
+++ app.rb
@@ -1,3 +1,9 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 
+class Wiki < Sinatra::Base
+  register Sinatra::ActiveRecordExtension
+  set :database_file, 'config/database.yml'
+
+  run! if app_file == $PROGRAM_NAME
+end
