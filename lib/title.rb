976ec8a587e2fccddb64aa6a35ff5e3686cdd85c
yasmine.morawej@gmail.com <yasmine.morawej@gmail.com>
2020-02-11 12:50:11 +0000

    test title character limit of 50 characters

--- lib/title.rb
+++ lib/title.rb
@@ -2,8 +2,8 @@ class Title < ActiveRecord::Base
   has_many :revisions
   validates :title, length: { maximum: 50 }
 
-  def initialize(title)
-    @title = title
+  def self.validate(title)
+    raise "Title invalid: exceeds 50 characters" if title.length > 50
+    title
   end
-
 end
--- spec/title_spec.rb
+++ spec/title_spec.rb
@@ -1,5 +1,7 @@
 require "title" 
 
 describe Title do
-  
+  it "must be under 50 characters long" do
+    expect{ Title.validate("a" * 100) }.to raise_error "Title invalid: exceeds 50 characters"
+  end 
 end
