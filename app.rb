e61e6753fd0903a2b339344b191c8df37af69cd2
yasmine.morawej@gmail.com <yasmine.morawej@gmail.com>
2020-02-12 13:47:15 +0000

    fix last rubocop offense

--- app.rb
+++ app.rb
@@ -14,16 +14,14 @@ class Wiki < Sinatra::Base
 
   get '/documents/:title' do
     title = Title.format_title(params[:title])
-    if Title.where(:title => title).blank?
-      return status 400
-    else
-      document_id = Title.find_id(params[:title])
-      entries = Revision.where(title_id: document_id)
-      content = []
-      entries.each { |entry| content.push(entry.content) }
-      p 'Revisions: ' + content.join(' , ')
+    return status 400 if Title.where(title: title).blank?
+
+    document_id = Title.find_id(params[:title])
+    entries = Revision.where(title_id: document_id)
+    content = []
+    entries.each { |entry| content.push(entry.content) }
+    p 'Revisions: ' + content.join(' , ')
   end
-end
 
   post '/documents/:title' do
     content = request.body.read
